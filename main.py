154377e39ba5d49ca81de4c28d7d3535c033d78a
liamgines <109954426+liamgines@users.noreply.github.com>
2025-04-11 00:17:57 -0700

    Identify features and target, then allocate some data for training and testing respectively

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+DATA_FILE_NAME = "Churn_Modelling.csv"
+TARGET_COLUMN_NAME = "Exited"
+
 def main():
-    print("Hello World")
+    data = pd.read_csv(DATA_FILE_NAME)
+
+    x = data.drop(columns=[TARGET_COLUMN_NAME])
+    y = data[TARGET_COLUMN_NAME]
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.8, train_size=0.2)
 
 main()
