da950f99182a2e441d0e7d299936f761090efe23
liamgines <109954426+liamgines@users.noreply.github.com>
2025-04-11 18:41:26 -0700

    Plot correlation matrix

--- main.py
+++ main.py
@@ -23,6 +23,19 @@ def box_plot_frame(data_frame):
     plt.xticks(rotation=60)
     plt.show()
 
+def correlation_plot_frame(data_frame, feature_names):
+    correlation_matrix = data_frame.corr()
+    figure = plt.figure()
+    axes = figure.add_subplot(111)
+    axes_image = axes.matshow(correlation_matrix, interpolation="nearest")
+    figure.colorbar(axes_image)
+    axes.set_xticks([i for i in range(len(feature_names))])
+    axes.set_yticks([i for i in range(len(feature_names))])
+    axes.set_xticklabels(feature_names)
+    axes.set_yticklabels(feature_names)
+    plt.xticks(rotation=90)
+    plt.show()
+
 def main():
     data_frame = pd.read_csv(DATA_FILE_NAME)
     data_frame = data_frame.drop(columns=IRRELEVANT_COLUMN_NAMES)
@@ -42,6 +55,8 @@ def main():
     data_frame = pd.DataFrame(data=scaled_data, columns=feature_names)
 
     box_plot_frame(data_frame)
+    
+    correlation_plot_frame(data_frame, feature_names)
 
     x = data_frame.drop(columns=[TARGET_COLUMN_NAME])
     y = data_frame[TARGET_COLUMN_NAME]
