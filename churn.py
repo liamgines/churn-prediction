eeaa2a0c213b0a3af56397e590583da30474394f
liamgines <109954426+liamgines@users.noreply.github.com>
2025-05-02 00:28:21 -0700

    Update churn.py to match notebook, add global random state variable

--- churn.py
+++ churn.py
@@ -10,16 +10,9 @@ CSV_FILE = "Churn_Modelling.csv"
 TARGET = "Exited"
 UNNECESSARY_FEATURES = ["RowNumber", "CustomerId", "Surname"]
 NOMINAL_FEATURES = ["Surname", "Geography", "Gender"]
+RANDOM_STATE = 42
 
 data_frame = pd.read_csv(CSV_FILE)
-
-x = data_frame.drop(columns=[TARGET])
-x = x.drop(columns=UNNECESSARY_FEATURES)
-y = data_frame[TARGET]
-
-data_frame.plot(kind="box")
-plt.xticks(rotation=90)
-plt.show()
 
 label_encoder = LabelEncoder()
 for nominal_feature in NOMINAL_FEATURES:
@@ -27,15 +20,25 @@ for nominal_feature in NOMINAL_FEATURES:
         pass
 
     else:
-        current_feature_values = x[nominal_feature]
+        current_feature_values = data_frame[nominal_feature]
         label_encoder.fit(current_feature_values)
-        x[nominal_feature] = label_encoder.transform(current_feature_values)
+        data_frame[nominal_feature] = label_encoder.transform(current_feature_values)
 
-def evaluate_model(model):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, train_size=0.8)
+data_frame = data_frame.drop(columns=UNNECESSARY_FEATURES)
+
+data_frame.plot(kind="box")
+plt.xticks(rotation=90)
+plt.show()
+
+x = data_frame.drop(columns=[TARGET])
+y = data_frame[TARGET]
+
+def test_model(model):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, train_size=0.8, random_state=RANDOM_STATE, stratify=y)
     model.fit(x_train, y_train)
     y_test_predicted = model.predict(x_test)
 
     print(classification_report(y_test, y_test_predicted))
+    return model
     
-evaluate_model(DecisionTreeClassifier())
+test_model(DecisionTreeClassifier(random_state=RANDOM_STATE))
