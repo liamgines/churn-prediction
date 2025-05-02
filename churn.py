f558dfa52c22dee8e6f4e63d6c203c8423e7ccd0
liamgines <109954426+liamgines@users.noreply.github.com>
2025-05-01 19:10:09 -0700

    Check for missing values

--- churn.py
+++ churn.py
@@ -5,6 +5,7 @@ TARGET = "Exited"
 UNNECESSARY_FEATURES = ["RowNumber", "CustomerId", "Surname"]
 
 data_frame = pd.read_csv(CSV_FILE)
+print(data_frame.isnull().sum())
 
 x = data_frame.drop(columns=[TARGET])
 y = data_frame[TARGET]
